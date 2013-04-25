856137db22fa8b36464b4445c28fbe4a8a5c0bc0
Adam Presley <adam@adampresley.com>
2013-04-25 12:02:21 -0500

    Added keymappings, a settings file, and got the structure for the text filters worked out

--- Blogify.py
+++ Blogify.py
@@ -19,14 +19,52 @@
 # limitations under the License.
 
 import sublime, sublime_plugin
+import re
+import inspect
 
-class ViewInBrowserCommand(sublime_plugin.TextCommand):
+class Blogify(sublime_plugin.TextCommand):
 	def run(self, edit):
+		#
+		# Filter definitions
+		#
+		self._filters = (
+			("(?i)&", "&amp;"),
+			("(?i)<", "&lt;"),
+			("(?i)>", "&gt;"),
+			("(?im)^\s+(.*?)", self._filter_leadingSpaces),
+		)
+
+		self._settings = sublime.load_settings("Blogify.sublime-settings")
+
 		#
 		# Get the contents of the current view
 		#
 		region = sublime.Region(0, self.view.size())
 		text = self.view.substr(region)
 
-		print text
+		replacedText = self._applyFilters(subject=text)
+		print replacedText
+
+	def _applyFilters(self, subject):
+		result = subject
 		
+		for f in self._filters:
+			pattern, replacement = f
+
+			#
+			# Is the replacement a string or a method call?
+			#
+			if "im_func" in dir(replacement):
+				result = replacement(pattern=pattern, subject=result)
+			else:
+				result = re.sub(pattern, replacement, result)
+
+		return result
+
+	def _filter_leadingSpaces(self, pattern, subject):
+		replacement = ""
+
+		for i in range(self._settings.get("indentNumSpaces")):
+			replacement += " "
+
+		return re.sub(pattern, replacement, subject)
